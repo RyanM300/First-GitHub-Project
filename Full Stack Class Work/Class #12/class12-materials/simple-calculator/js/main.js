1976edba5c687d80d84c8bd88298c931de1c2e65
Ryan McDonald <101738343+RyanM300@users.noreply.github.com>
2022-04-01 14:54:28 -0400

    some extra work

--- Full Stack Class Work/Class #12/class12-materials/simple-calculator/js/main.js
+++ Full Stack Class Work/Class #12/class12-materials/simple-calculator/js/main.js
@@ -4,6 +4,7 @@ document.querySelector('#pumpkin').addEventListener('click', makeZero)
 document.querySelector('#dominosPizza').addEventListener('click', jumanji)
 document.querySelector('#zebra').addEventListener('click', add9)
 document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
+document.querySelector('#homework').addEventListener('click', add10)
 
 function makeZero() {
   total = 0
@@ -23,4 +24,9 @@ function add9() {
 function sub2() {
   total = total - 2
   document.querySelector('#placeToPutResult').innerHTML = total
+}
+
+function add10() {
+  total = total + 10
+  document.querySelector('#placeToPutResult').innerText = total
 }
