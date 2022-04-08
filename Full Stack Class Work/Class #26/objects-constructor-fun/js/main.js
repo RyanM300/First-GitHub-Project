9891753242b37fa375bb5196b28c3e372b2de60f
Ryan McDonald <101738343+RyanM300@users.noreply.github.com>
2022-04-07 21:37:01 -0400

    api work

--- Full Stack Class Work/Class #26/objects-constructor-fun/js/main.js
+++ Full Stack Class Work/Class #26/objects-constructor-fun/js/main.js
@@ -1,1 +1,18 @@
 //Create a constructor with 4 properties and 3 methods
+function MakePizza (pizzaTopping , pizzaSauce, pizzaType, pizzaSize){
+    this.pizzaTopping = pizzaTopping
+    this.pizzaSauce = pizzaSauce
+    this.pizzaType = pizzaType
+    this.pizzaSize = pizzaSize
+
+    this.estimatedTime = function(){
+        console.log('Calculating....')
+    }
+    this.burntMouth = function(){
+        console.log('MAMMAAA MIA!!!')
+    }
+    this.frisbee = function(){
+        console.log('Heads up loser!')
+    }
+}
+let pizza = new MakePizza('peporni', 'marinara', 'pan', 'yugeee')
--- Full Stack Class Work/Class #26/objects-shots/js/main.js
+++ Full Stack Class Work/Class #26/objects-shots/js/main.js
@@ -1,1 +1,16 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+/*The user will enter a cocktail. Get a cocktail name, photo, and instructions and 
+place them in the DOM*/
+document.querySelector('button').addEventListener('click', getDrink)
+
+function getDrink(){
+    let drink = document.querySelector('input').value
+
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${getDrink}'")
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+         console.log(`error ${err}`)
+    });
+}
