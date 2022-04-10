53ba78f86a9f6d6f4c30465d5e95428e3cea4a41
Ryan McDonald <101738343+RyanM300@users.noreply.github.com>
2022-04-09 23:52:30 -0400

    nasa

--- Java Projects/Api Website/api.js
+++ Java Projects/Api Website/api.js
@@ -1,12 +1,12 @@
 document.querySelector('button').addEventListener('click', getPhoto)
 
 function getPhoto(){
-    const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+    const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY'
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-        console.log(data.hdurl)
-        document.querySelector('img').src = data.hdurl
+        console.log(data.photos[0].img_src)
+        document.querySelector('img').src = data.photos[0].img_src
         })
         .catch(err => {
             console.log(`error ${err}`)
