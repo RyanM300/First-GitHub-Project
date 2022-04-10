8547b80ff04211707ad08222b6b6e860415b1b05
Ryan McDonald <101738343+RyanM300@users.noreply.github.com>
2022-04-10 19:57:58 -0400

    project finished

--- Java Projects/Api Website/api.js
+++ Java Projects/Api Website/api.js
@@ -3,13 +3,15 @@ let search = document.querySelector('input').value
 
 function getPhoto() {
     let search = document.querySelector('input').value
-    let url = `https://api.nasa.gov/planetary/apod?date= + ${search}`
+    let url = `https://api.nasa.gov/planetary/apod?api_key=fMVaDz87DNEZ7ao5gJ19tvzrP28ttbqZn9qFIcTW&date=${search}`
     
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-        console.log(data.hdurl)
+        console.log(data)
         document.querySelector('img').src = data.hdurl
+        document.getElementById('description').style.visibility = "visible"
+        document.getElementById('description').innerText = data.explanation
         })
         .catch(err => {
             console.log(`error ${err}`)
