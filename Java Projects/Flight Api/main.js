e310574a870dbbdc2d46b2855cc2dcc247286953
Ryan McDonald <101738343+RyanM300@users.noreply.github.com>
2022-04-20 22:37:43 -0400

    .

--- Java Projects/Flight Api/main.js
+++ Java Projects/Flight Api/main.js
@@ -12,9 +12,20 @@ function getPlanes() {
     let lati = document.querySelector('#latitude').value
     let longi = document.querySelector('#longitude').value
 
-        fetch(`https://adsbexchange-com1.p.rapidapi.com/v2/lat/${lati}/lon/${longi}/dist/100/`, options)
-            .then(response => response.json())
-            .then(response => console.log(response))
-            .catch(err => console.error(err)
-        );
-}
+    fetch(`https://adsbexchange-com1.p.rapidapi.com/v2/lat/${lati}/lon/${longi}/dist/100/`, options)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.ac)
+
+            let arrayPlanes = data.ac
+            let arrayLength = arrayPlanes.length;
+            for (let i = 0; i < arrayLength; i++) {
+                console.log(arrayPlanes[i].flight);
+                if(arrayPlanes[i].flight == 'undefined'){
+                    console.log('no')
+                }else{
+                    document.createElement('p').innerText = arrayPlanes[i].flight
+                }
+            } 
+        ;
+})}
