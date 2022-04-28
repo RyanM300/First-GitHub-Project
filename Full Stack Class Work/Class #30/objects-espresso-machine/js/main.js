0d5827526f3e0fef8957585ab454af3992e98ab4
Ryan McDonald <101738343+RyanM300@users.noreply.github.com>
2022-04-28 19:26:33 -0400

    class work

--- Full Stack Class Work/Class #30/objects-espresso-machine/js/main.js
+++ Full Stack Class Work/Class #30/objects-espresso-machine/js/main.js
@@ -1,1 +1,19 @@
-//Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+//Create an espresso machine class that makes machines with 4 properties and 3 methods
+class Espresso{
+    constructor (machineColor, coffeeHandle, amountOfCoffee, sizeOfCup) {
+        this.color = machineColor
+        this.handle = coffeeHandle
+        this.coffee = amountOfCoffee
+        this.cup = sizeOfCup
+    }
+    brew() {
+        alert('coffee is made')
+    }
+    warmUp() {
+        alert('water is getting hot')
+    }
+    finished() {
+        alert('drink up')
+    }    
+}
+let myCoffee = new Espresso('black', 'big', 'double espresso', '12oz')
